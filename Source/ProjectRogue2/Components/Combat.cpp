31b1f6566d08da2930b9d21f959b333098fd85fa
Thomas Jensen <38362240+hrimolf@users.noreply.github.com>
2022-06-07 19:35:09 -0700

    Updated Combat component for equipment

--- Source/ProjectRogue2/Components/Combat.cpp
+++ Source/ProjectRogue2/Components/Combat.cpp
@@ -2,8 +2,10 @@
 
 
 #include "Combat.h"
-#include "CharacterStats.h"
 #include "../Characters/BaseCharacter.h"
+#include "../Item.h"
+#include "CharacterStats.h"
+#include "Equipment.h"
 
 // Sets default values for this component's properties
 UCombat::UCombat()
@@ -28,20 +30,17 @@ void UCombat::BeginPlay()
 void UCombat::TakeDamage(int Amount)
 {
 	//get armor
-	//UActorComponent* pComponent = Owner->GetComponentByClass(UEquipment::StaticClass());
-	//UEquipment* pEquipment = Cast<UEquipment>(pComponent); 
-	int Armor = 0; //pEquipment->GetTotalArmor();
+	UActorComponent* pComponent = Owner->GetComponentByClass(UEquipment::StaticClass());
+	UEquipment* pEquipment = Cast<UEquipment>(pComponent); 
+	int Armor = pEquipment->GetTotalArmor();
 
-	//goes in UEquipment::GetTotalArmor
-	//for (int32 i = 0; i < (int)EEquipmentSlot::Count; ++i)
-	//{
-	// if (AItem* pItem = EquippedItems[(EEquipmentSlot)i])
-	// {
-	//  Armor += pItem->GetArmor();
-	// }
-	//}
-
-	CurrentHealth -= (Amount - Armor);
+	int Damage = (Amount - Armor);
+	//make sure that at least 1 damage is always dealt
+	if (Damage <= 0)
+	{
+		Damage = 1;
+	}
+	CurrentHealth -= Damage;
 	if (CurrentHealth < 0)
 	{
 		CurrentHealth = 0;
@@ -50,7 +49,9 @@ void UCombat::TakeDamage(int Amount)
 
 void UCombat::Heal(int Amount)
 {
-	CurrentHealth += Amount;
+	UActorComponent* pComponent = Owner->GetComponentByClass(UCharacterStats::StaticClass());
+	UCharacterStats* pStats = Cast<UCharacterStats>(pComponent);
+	CurrentHealth += Amount + pStats->GetValue(EStats::Intelligence);
 	if (CurrentHealth > MaxHealth)
 	{
 		CurrentHealth = MaxHealth;
@@ -61,38 +62,24 @@ void UCombat::Attack(ABaseCharacter* Target)
 {
 	UActorComponent* pComponent = nullptr;
 	int Damage = 0;
-	//pComponent = Owner->GetComponentByClass(UEquipment::StaticClass());
-	//check(pComponent);
-	//UEquipment* pEquipment = Cast<UEquipment>(pComponent);
-	//check(pEquipment);
-	//AItem* pLeftHand = pEquipment->GetSlot(EEquipmentSlot::LeftHand);
-	//AItem* pRightHand = pEquipment->GetSlot(EEquipmentSlot::RightHand);
+	pComponent = Owner->GetComponentByClass(UEquipment::StaticClass());
+	check(pComponent);
+	UEquipment* pEquipment = Cast<UEquipment>(pComponent);
+	check(pEquipment);
+	AItem* pLeftHand = pEquipment->GetSlot(EEquipmentSlot::LeftHand);
+	AItem* pRightHand = pEquipment->GetSlot(EEquipmentSlot::RightHand);
 	pComponent = Owner->GetComponentByClass(UCharacterStats::StaticClass());
 	UCharacterStats* pStats = Cast<UCharacterStats>(pComponent);
-	//if (pLeftHand)
-	//{
-	//Damage += pLeftHand->GetDamage() + pStats->GetValue(pLeftHand->GetStatType());
-	//}
-	//if (pRightHand)
-	//{
-	//Damage += pRightHand->GetDamage() + pStats->GetValue(pRightHand->GetStatType());
-	//
-	//} 
+	if (pLeftHand)
+	{
+		Damage += pLeftHand->GetDamage() + pStats->GetValue(pLeftHand->GetStatType());
+	}
+	if (pRightHand)
+	{
+		Damage += pRightHand->GetDamage() + pStats->GetValue(pRightHand->GetStatType());
+	} 
 
 	pComponent = Target->GetComponentByClass(UCombat::StaticClass());
 	UCombat* pCombat = Cast<UCombat>(pComponent);
 	pCombat->TakeDamage(Damage);
-}
-
-int UCombat::GetStatModifier(/*AItem* pItem*/)
-{
-	//EStats StatType = pItem->GetStatType();
-	//return pStats->GetValue(EStats::Strength);
-	//if(StatType == EStats::Strength)
-	//{
-	//}
-	//else if (StatType == EStats::Dexterity)
-	//{
-	//return pStats->GetValue(EStats::Dexterity)
-	//}
 }
